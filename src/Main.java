8a22e92d98926fee87cff5b169445e5fcfdb5490
Artem S <saltem@inbox.ru>
2024-06-07 09:05:41 +0300

    Изменена логика программы: программа считывает прописанный пользователем путь и определяет путь к файлу или путь к директории. Если к файлу - ведет подсчет указанных путей к файлам.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,42 @@
+import java.io.File;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите целое число a: ");
-        int number = new Scanner(System.in).nextInt();
-        System.out.println("Введите целое число b: ");
-        int number2 = new Scanner(System.in).nextInt();
-        int add = number + number2;
-        int sub = number - number2;
-        int mult = number * number2;
-        double div = (double) number / number2;
-        System.out.println("a + b = " + add);
-        System.out.println("a - b = " + sub);
-        System.out.println("a * b = " + mult);
-        System.out.println("a / b = " + div);
-
+        System.out.println("Укажите путь к файлу или к директории");
+        String path = new Scanner(System.in).nextLine();//Запрос путь в консоли
+        File file = new File(path);
+        int numOfReq = 0;
+        boolean fileExists = file.exists();
+        boolean isDirectory = file.isDirectory();
+        String a = "Stop";
+        while (path!= a) {
+            if (fileExists == false) {
+                System.out.println("Файл не существует или указанный путь является путём к папке");
+                path = new Scanner(System.in).nextLine();
+                file = new File(path);
+                fileExists = file.exists();
+                isDirectory = file.isDirectory();
+                continue;
+            }
+            if (isDirectory == false) {
+                System.out.println("Путь указан верно");
+                numOfReq = numOfReq + 1;
+                System.out.println("Это файл номер " + numOfReq);
+                path = new Scanner(System.in).nextLine();
+                file = new File(path);
+                fileExists = file.exists();
+                isDirectory = file.isDirectory();
+                continue;
+            }
+            if (isDirectory == true) {
+                System.out.println("Файл не существует или указанный путь является путём к папке");
+                path = new Scanner(System.in).nextLine();
+                file = new File(path);
+                fileExists = file.exists();
+                isDirectory = file.isDirectory();
+            }
+        }
     }
 }
